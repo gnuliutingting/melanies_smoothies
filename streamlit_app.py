b663b470c4570609b7bac40ae5c10848114c0743
Tingting Liu <gnuliutingting@gmail.com>
2024-11-20 15:56:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,4 +59,9 @@ if ingredients_list:
 
         # Show success message
         st.success(f"Your Smoothie is ordered,{name_on_order}!", icon="✅")
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
         
