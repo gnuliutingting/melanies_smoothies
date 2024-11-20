71dc724f4e5ed2973a137e30d2eece0be695dc99
Tingting Liu <gnuliutingting@gmail.com>
2024-11-20 16:24:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,29 +43,29 @@ if ingredients_list:
          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ingredients_list)
          sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width = True)
 
-    # Show selected ingredients
-    st.write(f"You have selected: {ingredients_string}")
-
-    # Insert the ingredients into Snowflake when the submit button is pressed
-    time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
-
-
-        # Sanitize ingredients_string and name_on_order for SQL execution by quoting the values
-        ingredients_string = f"'{ingredients_string}'"
-        name_on_order = f"'{name_on_order}'"
-        # SQL Insert statement using placeholders to prevent SQL injection
-        my_insert_stmt = f""" 
-            INSERT INTO smoothies.public.orders(ingredients, name_on_order) 
-            VALUES ({ingredients_string}, {name_on_order})
-        """
-        
-        # Execute the SQL statement
-        session.sql(my_insert_stmt).collect()
-
-        # Show success message
-        st.success(f"Your Smoothie is ordered,{name_on_order}!", icon="✅")
+          # Show selected ingredients
+          st.write(f"You have selected: {ingredients_string}")
+      
+          # Insert the ingredients into Snowflake when the submit button is pressed
+          time_to_insert = st.button('Submit Order')
+      
+          if time_to_insert:
+      
+      
+              # Sanitize ingredients_string and name_on_order for SQL execution by quoting the values
+              ingredients_string = f"'{ingredients_string}'"
+              name_on_order = f"'{name_on_order}'"
+              # SQL Insert statement using placeholders to prevent SQL injection
+              my_insert_stmt = f""" 
+                  INSERT INTO smoothies.public.orders(ingredients, name_on_order) 
+                  VALUES ({ingredients_string}, {name_on_order})
+              """
+              
+              # Execute the SQL statement
+              session.sql(my_insert_stmt).collect()
+      
+              # Show success message
+              st.success(f"Your Smoothie is ordered,{name_on_order}!", icon="✅")
 
 
 
