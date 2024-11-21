7405bb9dcc16d370b026461bcbabbf0525db50aa
Tingting Liu <gnuliutingting@gmail.com>
2024-11-21 09:50:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredient_string =''
       
-   for fruit_chosen in ingredients_list:
-         ingredient_string += fruit_chosen +''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen +' '
          st.subheader(ingredients_list + 'Nutriention Information') 
          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ingredients_list)
          sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width = True)
