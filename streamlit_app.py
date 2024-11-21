5f5feca267197a70cf32081d03eda05e00808ecd
Tingting Liu <gnuliutingting@gmail.com>
2024-11-21 10:44:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,14 @@ if ingredients_list:
 time_to_insert = st.button('Submit Order')
       
 if time_to_insert:
+# Sanitize ingredients_string and name_on_order for SQL execution by quoting the values
+   ingredients_string = f"'{ingredients_string}'"
+   name_on_order = f"'{name_on_order}'"
+# SQL Insert statement using placeholders to prevent SQL injection
+   my_insert_stmt = f""" 
+   INSERT INTO smoothies.public.orders(ingredients, name_on_order) 
+   VALUES ({ingredients_string}, {name_on_order})
+       """
               
  # Execute the SQL statement
    session.sql(my_insert_stmt).collect()
