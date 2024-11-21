531c952f271ea159986f175f5067c446fcf4f220
Tingting Liu <gnuliutingting@gmail.com>
2024-11-21 12:19:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,12 @@ session = cnx.session()
 
 # Get the list of fruits from Snowflake table
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')).collect()
-st.dataframe(data = my_dataframe, use_container_width = True)
+#st.dataframe(data = my_dataframe, use_container_width = True)
+#st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 # Extract fruit names into a list
